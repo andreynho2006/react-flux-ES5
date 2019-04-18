c76a4ceb344e1c29f75b9e6498d046a522ac4388
andreynho2006 <andreynho2006@gmail.com>
2019-04-18 20:51:19 +0300

    task to handle html files and move them to dist

--- gulpfile.js
+++ gulpfile.js
@@ -28,4 +28,10 @@ gulp.task("connect", function() {
 gulp.task("open", ["connect"], function() {
     gulp.src("dist/index.html")
         .pipe(open("", { url: config.devBaseUrl + ":" + config.port + "/"}));
-}];
+});
+
+gulp.task("html", function() {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
