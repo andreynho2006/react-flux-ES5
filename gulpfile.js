a40002c13a99be7fd3ee4c3c535a7f27269ad3c2
andreynho2006 <andreynho2006@gmail.com>
2019-04-18 21:49:09 +0300

    refactor gulpfile v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
 "use strict";
 
-var gulp = require("gulp");
-var connect = require("gulp-connect");    // run a local server
-var gulp = require("gulp-open");          // open a url and a web browser
+var gulp = require('gulp');
+var connect = require('gulp-connect');    // run a local server
+var open = require('gulp-open');          // open a url and a web browser
 
-var confiig = {
+var config = {
     port: 9005,
-    devBaseUrl: "http://localhost",
+    devBaseUrl: 'http://localhost',
     paths: {
-        html: "./src/*.html",
-        dist: "./dist"
+        html: './src/*.html',
+        dist: './dist'
     }
 }
 
 
 // Start a local development server
-gulp.task("connect", function() {
+gulp.task('connect', gulp.series(function() {
     connect.server({
-        root: ["dist"],
+        root: ['dist'],
         port: config.port,
         base: config.devBaseUrl,
         livereload: true
     });
-});
+}));
 
 // open a given file
-gulp.task("open", ["connect"], function() {
-    gulp.src("dist/index.html")
-        .pipe(open("", { url: config.devBaseUrl + ":" + config.port + "/"}));
-});
+gulp.task('open', gulp.series(['connect'], function() {
+    gulp.src('dist/index.html')
+        .pipe(open('', { url: config.devBaseUrl + ':' + config.port + '/'}));
+}));
 
-gulp.task("html", function() {
+gulp.task('html', gulp.series(function() {
     gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
-});
+}));
+
+gulp.task('default', gulp.parallel('html', 'open'));
