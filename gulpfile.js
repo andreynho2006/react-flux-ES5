fe9c3a8d54a2f0a2e764dfdc1db153f2d130885e
andreynho2006 <andreynho2006@gmail.com>
2019-04-18 20:40:33 +0300

    init a new project

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var gulp = require("gulp");
+var connect = require("gulp-connect");    // run a local server
+var gulp = require("gulp-open");          // open a url and a web browser
