e1de680b006eb0eef30ab74f8e9613777af32526
andreynho2006 <andreynho2006@gmail.com>
2019-04-18 20:47:22 +0300

    make a gulp task to open a development server and open a file

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,29 @@
 var gulp = require("gulp");
 var connect = require("gulp-connect");    // run a local server
 var gulp = require("gulp-open");          // open a url and a web browser
+
+var confiig = {
+    port: 9005,
+    devBaseUrl: "http://localhost",
+    paths: {
+        html: "./src/*.html",
+        dist: "./dist"
+    }
+}
+
+
+// Start a local development server
+gulp.task("connect", function() {
+    connect.server({
+        root: ["dist"],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+});
+
+// open a given file
+gulp.task("open", ["connect"], function() {
+    gulp.src("dist/index.html")
+        .pipe(open("", { url: config.devBaseUrl + ":" + config.port + "/"}));
+}];
