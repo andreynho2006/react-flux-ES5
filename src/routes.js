911e37fb29bb627901f2b61a18e49d858cb2ef4b
andreynho2006 <andreynho2006@gmail.com>
2019-04-21 11:55:46 +0300

    add addAuthor page to routes

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,18 @@ var Router = require('react-router');
 var DefaultRoute = Router.defaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')} />
         <Route name="authors" handler={require('./components/authors/authorPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
+        <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
         <NotFoundRoute handler={require('./components/notFoundPage')} />
+        <Redirect to="about" from="about-us" />
+        <Redirect from="awhurs" to="authors" />
+        <Redirect from="about/*" to="about" />
     </Route>
 );
 
