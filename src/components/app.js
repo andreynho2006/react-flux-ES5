da02e7b78ea36b91c7200441e01d54f4178cb09a
andreynho2006 <andreynho2006@gmail.com>
2019-04-21 12:50:06 +0300

    refactor

--- src/components/app.js
+++ src/components/app.js
@@ -17,3 +17,5 @@ var App = React.createClass({
         );
     }
 });
+
+module.exports = App;
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -2,7 +2,7 @@
 
 var React = require('react');
 var Router = require("react-router")
-var link = Router.Link;
+var Link = Router.Link;
 
 var Header = React.createClass({
     render: function() {
