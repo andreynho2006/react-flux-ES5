1f274f150047956f470f06cb328e693fcb0050f5
andreynho2006 <andreynho2006@gmail.com>
2019-04-21 12:35:48 +0300

    refactor and add Input component with props to author form

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require('react');
+var Input = require('../common/textInput')
 
 var AuthorForm = React.createClass({
 
@@ -9,25 +10,17 @@ var AuthorForm = React.createClass({
         return (
             <form>
                 <h1>Manage Author</h1>
-                <label htmlfor="firstName">First Name</label>
-                <input type="text"
-                        name="firstName"
-                        className="form-control"
-                        placeholder="First name"
-                        ref="FirstName"
-                        onChange={this.props.onChange}
-                        value={this.props.authorForm.firstName} />
-                <br />
-
-                <label htmlfor="lastName">Last Name</label>
-                <input type="text"
-                        name="lastName"
-                        className="form-control"
-                        placeholder="Lastt name"
-                        ref="LastName"
-                        onChange={this.props.onChange}
-                        value={this.props.author.lastName} />
-                <br />
+                <Input
+                    name="firstName"
+                    label="First Name"
+                    value={this.props.author.firstName}
+                    onChange={this.props.onChange} />
+                
+                <Input
+                    name="lastName"
+                    label="Last Name"
+                    value={this.props.author.lastName}
+                    onChange={this.props.onChange} />
 
                 <input type="submit" value="Save" className="btn btn-defaut" />
             </form>
