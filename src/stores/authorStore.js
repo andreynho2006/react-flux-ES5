dac49f1510f44f9bdf9b32fae0cff325537d4bf1
andreynho2006 <andreynho2006@gmail.com>
2019-04-21 15:06:50 +0300

    make a new folder to hold stores and make authorStore

---
+++ src/stores/authorStore.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmmiter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
+
+var AuthorStore = assign({}, EventEmitter.prototype, {
+
+   addChangeListener: function(callback) {
+       this.on(CHANGE_EVENT, callback);
+   },
+
+   removeChangeListener: function(callback) {
+       this.removeChangeListener(CHANGE_EVENT, callback);
+   },
+
+   emitChange: function() {
+       this.emit(CHANGE_EVENT);
+
+   }
+});
